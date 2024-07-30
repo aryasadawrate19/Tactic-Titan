f3d4551b38ea3d539615bfc2bfd18db4e8e6105b
aryasadawrate19 <aarya.sadawrate@gmail.com>
2024-07-31 00:38:29 +0530

    Near Final commit

---
+++ core/ChessMain.py
@@ -0,0 +1,15 @@
+import pygame as p
+import os
+from core import ChessController
+
+
+def main(language, subscribed):
+
+    controller = ChessController.ChessController()
+    play_sound = p.mixer.Sound(r"../images/menucut.mp3")
+    play_sound.play()
+    controller.playGame(language, subscribed)
+
+
+if __name__ == "__main__":
+    main("english", False)
